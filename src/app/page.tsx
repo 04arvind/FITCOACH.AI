97787a9fb0fc2d0fb31f1440d95c1ddd3829ed8c
04arvind <arvind42005@gmail.com>
2025-07-07 00:44:47 +0530

    feat(auth): complete authentication setup with login and signup functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
+"use client"
+import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
+import { SignOutButton } from "@clerk/nextjs";
+
 const HomePage = () => {
   return <div>
     HomePage
+    <SignedIn>
+    <SignOutButton/>
+
+    </SignedIn>
+    <SignedOut>
+    <SignInButton/>
+
+    </SignedOut>
   </div>  
 };
 
