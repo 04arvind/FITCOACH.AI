5b344ed98258fb2c01ca5b96cfd2da086c908b55
04arvind <arvind42005@gmail.com>
2025-07-06 20:26:24 +0530

    do basic setups and connect cleark

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from "@clerk/nextjs"
+
+const SignUpPage = () => {
+   return <main className="flex h-screen w-full items-center justify-center bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900">
+       {/* <SignIn /> */}
+        <div className="rounded-3xl shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] bg-white/10 backdrop-blur-md border border-white/20 p-10 max-w-md w-full transition-transform duration-500 hover:scale-105 items-center justify-center flex h-170 w-full">
+         <SignUp />
+       </div>
+       </main>
+}
+
+export default SignUpPage
